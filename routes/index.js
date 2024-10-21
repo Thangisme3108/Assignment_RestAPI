810ea0a42080becb94b8ab4dfeb84e2f7dbe1061
Thangisme3108 <phamnangthang02@gmail.com>
2024-10-21 21:13:34 +0700

    commit DatComTam

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,12 @@ connect(mongoURI, {
     console.log("Kết nối MongoDB thất bại", err);
 }));
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    ComTam.find({}).then((comtams) => {
-        res.render('index',  {title: 'Danh sách các món cơm tấm', comtams: comtams});
-    })
-});
+    /* GET home page. */
+    router.get('/', function (req, res) {
+        ComTam.find({}).then((comtams) => {
+            res.render('index',  {title: 'Danh sách các món cơm tấm', comtams: comtams});
+        })
+    });
 
     module.exports = router;
 
@@ -32,7 +32,7 @@ router.get('/', function (req, res) {
 
     const ComTam = mongoose.model("ComTam", ComTamSchema);
 
-    router.post('/chonComTam', function (req, res) {
+    router.post('/themComTam', function (req, res) {
         const tenMon = req.body.mon;
         const giaTien = req.body.price;
         // const soLuong = req.body.soLuong;
@@ -49,9 +49,8 @@ router.get('/', function (req, res) {
     })
 
     router.get('/them', function (req, res) {
-        res.render('chonComTam')
+        res.render('themComTam')
     })
-
 
     router.get('/DangNhap', function (req, res) {
         res.render('dangNhap', {title: 'Đăng nhập'});
@@ -78,49 +77,80 @@ router.get('/', function (req, res) {
             res.send(error)
         }))
     })
-    router.get('/ChonComTam', function (req, res) {
-        res.render('chonComTam', {title: 'Chọn cơm tấm'});
+    router.get('/ThemComTam', function (req, res) {
+        res.render('themComTam', {title: 'Chọn cơm tấm'});
     })
 
     router.get('/DatComTam', function (req, res) {
-        res.render('datComTam', {title: 'Đặt cơm tấm'});
+        ComTam.find({}).then((comtams) => {
+            console.log(comtams);
+            res.render('datComTam', {title: 'Đặt cơm tấm', comtams: comtams});
+        }).catch((error) => {
+            res.send('Lỗi khi lấy danh sách món: ' + error);
+        });
+        // res.render('datComTam', {title: 'Đặt cơm tấm'});
+    })
+
+    const DatMonComTamSchema = new Schema({
+        tenMon: String,
+        giaTien: Number,
+        soLuong: Number,
+    })
+
+    const DatMonComTam = mongoose.model('DatMonComTam', DatMonComTamSchema);
+
+    router.post('/DatComTam', function (req, res) {
+        const tenMon = req.body.tenMon;
+        const giaTien = req.body.giaTien;
+        const soLuong = req.body.soLuong;
+
+        const newDatMonComTam = new DatMonComTam({
+            tenMon: tenMon,
+            giaTien: giaTien,
+            soLuong: soLuong,
+        })
+        newDatMonComTam.save().then(() => {
+            res.send("Đặt món thành công: " + tenMon)
+        }).catch(error => {
+            res.send('Lỗi khi đặt món: ' + error);
+        });
     })
 
     router.get('/ChiTietDonHang', function (req, res) {
         res.render('chiTietDonHang', {title: 'Chi tiết đơn hàng'});
     })
 
-router.get('/xoa/:id', function (req, res){
-    const id = req.params.id;
-    ComTam.findByIdAndDelete(id).then((comtam) => {
-        res.send('Xoá thành công')
-    }).catch((error)=>{
-        res.send(error);
+    router.get('/xoa/:id', function (req, res){
+        const id = req.params.id;
+        ComTam.findByIdAndDelete(id).then((comtam) => {
+            res.send('Xoá thành công')
+        }).catch((error)=>{
+            res.send(error);
+        })
     })
-})
+    //
+    // router.get('/themComTam', function (req, res) {
+    //     res.render('themComTam')
+    // })
 
-router.get('/themComTam', function (req, res) {
-    res.render('themComTam')
-})
-
-router.get('/sua/:id', function (req, res) {
-    const id = req.params.id;
-    ComTam.findById(id).then((comtam) => {
-        res.render('suaComTam', {comtam: comtam});
+    router.get('/sua/:id', function (req, res) {
+        const id = req.params.id;
+        ComTam.findById(id).then((comtam) => {
+            res.render('suaComTam', {comtam: comtam});
+        })
     })
-})
 
-router.post('/suact/:id', function (req, res) {
-    const id = req.params.id;
-    const tenMon = req.body.mon;
-    const giaTien = req.body.price;
-    ComTam.findByIdAndUpdate(id, {
-        tenMon: tenMon,
-        giaTien: giaTien,
-    }).then((comtam) => {
-        res.send('Sửa thành công')
-    }).catch((error)=>{
-        res.send(error);
+    router.post('/suact/:id', function (req, res) {
+        const id = req.params.id;
+        const tenMon = req.body.mon;
+        const giaTien = req.body.price;
+        ComTam.findByIdAndUpdate(id, {
+            tenMon: tenMon,
+            giaTien: giaTien,
+        }).then((comtam) => {
+            res.send('Sửa thành công')
+        }).catch((error)=>{
+            res.send(error);
+        })
     })
-})
 
